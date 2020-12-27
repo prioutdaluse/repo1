429717e4e674e4ad1779cf9ac7943be4ec9016a4
kangkang <178674007@qq.com>
2020-12-27 16:07:59 +0800

    111

---
+++ helloproject/src/main/JavaTest01.java
@@ -0,0 +1,14 @@
+package main;
+
+/**
+ * @author kang
+ * @create 2020-10-07-9:28
+ */
+public class JavaTest01 {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 2;
+        double num3= num1 / num2;
+        System.out.println(num3);
+    }
+}
